267f3879f1a8de4d637b395ad9ca3ede12e4dd0c
chocotone__ <98628957+chocotoneg@users.noreply.github.com>
2024-11-26 17:24:40 -0300

    Translated system.lua entirely

--- coordinator/config/system.lua
+++ coordinator/config/system.lua
@@ -210,8 +210,8 @@ function system.create(tool_ctl, main_pane, cfg_sys, divs, ext, style)
         else path_err.show() end
     end
 
-    PushButton{parent=log_c_1,x=1,y=14,text="\x1b Back",callback=function()main_pane.set_value(6)end,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
-    PushButton{parent=log_c_1,x=44,y=14,text="Next \x1a",callback=submit_log,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
+    PushButton{parent=log_c_1,x=1,y=14,text="\x1bVoltar",callback=function()main_pane.set_value(6)end,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
+    PushButton{parent=log_c_1,x=44,y=14,text="Prox \x1a",callback=submit_log,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
 
     --#endregion
 
@@ -224,23 +224,23 @@ function system.create(tool_ctl, main_pane, cfg_sys, divs, ext, style)
 
     local clr_pane = MultiPane{parent=clr_cfg,x=1,y=4,panes={clr_c_1,clr_c_2,clr_c_3,clr_c_4}}
 
-    TextBox{parent=clr_cfg,x=1,y=2,text=" Color Configuration",fg_bg=cpair(colors.black,colors.magenta)}
+    TextBox{parent=clr_cfg,x=1,y=2,text=" Configura\xe7\xe3o de Cor",fg_bg=cpair(colors.black,colors.magenta)}
 
-    TextBox{parent=clr_c_1,x=1,y=1,height=2,text="Here you can select the color themes for the different UI displays."}
-    TextBox{parent=clr_c_1,x=1,y=4,height=2,text="Click 'Accessibility' below to access colorblind assistive options.",fg_bg=g_lg_fg_bg}
+    TextBox{parent=clr_c_1,x=1,y=1,height=2,text="Aqui voc\xea pode selecionar os temas de cores para as diferentes interfaces."}
+    TextBox{parent=clr_c_1,x=1,y=4,height=2,text="Clique em 'Acessibilidade' abaixo para acessar op\xe7\xf5es assistivas para daltonismo.",fg_bg=g_lg_fg_bg}
 
-    TextBox{parent=clr_c_1,x=1,y=7,text="Main UI Theme"}
+    TextBox{parent=clr_c_1,x=1,y=7,text="Tema Princ da UI"}
     local main_theme = RadioButton{parent=clr_c_1,x=1,y=8,default=ini_cfg.MainTheme,options=themes.UI_THEME_NAMES,callback=function()end,radio_colors=cpair(colors.lightGray,colors.black),select_color=colors.magenta}
 
-    TextBox{parent=clr_c_1,x=18,y=7,text="Front Panel Theme"}
+    TextBox{parent=clr_c_1,x=18,y=7,text="Tema do Painel Frontal"}
     local fp_theme = RadioButton{parent=clr_c_1,x=18,y=8,default=ini_cfg.FrontPanelTheme,options=themes.FP_THEME_NAMES,callback=function()end,radio_colors=cpair(colors.lightGray,colors.black),select_color=colors.magenta}
 
-    TextBox{parent=clr_c_2,x=1,y=1,height=6,text="This system uses color heavily to distinguish ok and not, with some indicators using many colors. By selecting a mode below, indicators will change as shown. For non-standard modes, indicators with more than two colors will usually be split up."}
+    TextBox{parent=clr_c_2,x=1,y=1,height=6,text="Este sistema usa cores para diferenciar status, com alguns indicadores usando v\xe1rias cores. Ao selecionar um modo abaixo, os indicadores mudar\xe3o conforme mostrado. Em modos n\xe3o padr\xe3o, indicadores com mais de duas cores geralmente ser\xe3o divididos."}
 
-    TextBox{parent=clr_c_2,x=21,y=7,text="Preview"}
-    local _ = IndLight{parent=clr_c_2,x=21,y=8,label="Good",colors=cpair(colors.black,colors.green)}
-    _ = IndLight{parent=clr_c_2,x=21,y=9,label="Warning",colors=cpair(colors.black,colors.yellow)}
-    _ = IndLight{parent=clr_c_2,x=21,y=10,label="Bad",colors=cpair(colors.black,colors.red)}
+    TextBox{parent=clr_c_2,x=21,y=7,text="Prever"}
+    local _ = IndLight{parent=clr_c_2,x=21,y=8,label="Bom",colors=cpair(colors.black,colors.green)}
+    _ = IndLight{parent=clr_c_2,x=21,y=9,label="Aviso",colors=cpair(colors.black,colors.yellow)}
+    _ = IndLight{parent=clr_c_2,x=21,y=10,label="Ruim",colors=cpair(colors.black,colors.red)}
     local b_off = IndLight{parent=clr_c_2,x=21,y=11,label="Off",colors=cpair(colors.black,colors.black),hidden=true}
     local g_off = IndLight{parent=clr_c_2,x=21,y=11,label="Off",colors=cpair(colors.gray,colors.gray),hidden=true}
 
@@ -264,12 +264,12 @@ function system.create(tool_ctl, main_pane, cfg_sys, divs, ext, style)
         end
     end
 
-    TextBox{parent=clr_c_2,x=1,y=7,width=10,text="Color Mode"}
+    TextBox{parent=clr_c_2,x=1,y=7,width=11,text="Modo de Cor"}
     local c_mode = RadioButton{parent=clr_c_2,x=1,y=8,default=ini_cfg.ColorMode,options=themes.COLOR_MODE_NAMES,callback=recolor,radio_colors=cpair(colors.lightGray,colors.black),select_color=colors.magenta}
 
-    TextBox{parent=clr_c_2,x=21,y=13,height=2,width=18,text="Note: exact color varies by theme.",fg_bg=g_lg_fg_bg}
+    TextBox{parent=clr_c_2,x=21,y=13,height=2,width=18,text="Aviso: as cores variam por tema.",fg_bg=g_lg_fg_bg}
 
-    PushButton{parent=clr_c_2,x=44,y=14,min_width=6,text="Done",callback=function()clr_pane.set_value(1)end,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
+    PushButton{parent=clr_c_2,x=44,y=14,min_width=6,text="Pronto",callback=function()clr_pane.set_value(1)end,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
 
     local function back_from_colors()
         main_pane.set_value(tri(tool_ctl.jumped_to_color, 1, 7))
@@ -308,10 +308,10 @@ function system.create(tool_ctl, main_pane, cfg_sys, divs, ext, style)
         end
     end
 
-    PushButton{parent=clr_c_1,x=1,y=14,text="\x1b Back",callback=back_from_colors,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
-    PushButton{parent=clr_c_1,x=8,y=14,min_width=15,text="Accessibility",callback=show_access,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
-    tool_ctl.color_next = PushButton{parent=clr_c_1,x=44,y=14,text="Next \x1a",callback=submit_colors,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
-    tool_ctl.color_apply = PushButton{parent=clr_c_1,x=43,y=14,min_width=7,text="Apply",callback=submit_colors,fg_bg=cpair(colors.black,colors.green),active_fg_bg=btn_act_fg_bg}
+    PushButton{parent=clr_c_1,x=1,y=14,text="\x1bVoltar",callback=back_from_colors,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
+    PushButton{parent=clr_c_1,x=9,y=14,min_width=15,text="Acessibilidade",callback=show_access,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
+    tool_ctl.color_next = PushButton{parent=clr_c_1,x=44,y=14,text="Prox \x1a",callback=submit_colors,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
+    tool_ctl.color_apply = PushButton{parent=clr_c_1,x=43,y=14,min_width=7,text="Aplicar",callback=submit_colors,fg_bg=cpair(colors.black,colors.green),active_fg_bg=btn_act_fg_bg}
 
     tool_ctl.color_apply.hide(true)
 
@@ -320,12 +320,12 @@ function system.create(tool_ctl, main_pane, cfg_sys, divs, ext, style)
         clr_pane.set_value(1)
     end
 
-    TextBox{parent=clr_c_3,x=1,y=1,text="Settings saved!"}
-    PushButton{parent=clr_c_3,x=1,y=14,min_width=6,text="Exit",callback=exit,fg_bg=cpair(colors.black,colors.red),active_fg_bg=cpair(colors.white,colors.gray)}
+    TextBox{parent=clr_c_3,x=1,y=1,text="Configura\xe7\xe3o salva!"}
+    PushButton{parent=clr_c_3,x=1,y=14,min_width=6,text="Sair",callback=exit,fg_bg=cpair(colors.black,colors.red),active_fg_bg=cpair(colors.white,colors.gray)}
     PushButton{parent=clr_c_3,x=44,y=14,min_width=6,text="Home",callback=c_go_home,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
 
-    TextBox{parent=clr_c_4,x=1,y=1,height=5,text="Failed to save the settings file.\n\nThere may not be enough space for the modification or server file permissions may be denying writes."}
-    PushButton{parent=clr_c_4,x=1,y=14,min_width=6,text="Exit",callback=exit,fg_bg=cpair(colors.black,colors.red),active_fg_bg=cpair(colors.white,colors.gray)}
+    TextBox{parent=clr_c_4,x=1,y=1,height=5,text="N\xe3o foi poss\xedvel salvar as configura\xe7\xf5es.\n\nPode n\xf5o haver espa\xe7o suficiente ou as permiss\xf5es do servidor podem estar negando grava\xe7\xf5es."}
+    PushButton{parent=clr_c_4,x=1,y=14,min_width=6,text="Sair",callback=exit,fg_bg=cpair(colors.black,colors.red),active_fg_bg=cpair(colors.white,colors.gray)}
     PushButton{parent=clr_c_4,x=44,y=14,min_width=6,text="Home",callback=c_go_home,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
 
     --#endregion
@@ -339,7 +339,7 @@ function system.create(tool_ctl, main_pane, cfg_sys, divs, ext, style)
 
     local sum_pane = MultiPane{parent=summary,x=1,y=4,panes={sum_c_1,sum_c_2,sum_c_3,sum_c_4}}
 
-    TextBox{parent=summary,x=1,y=2,text=" Summary",fg_bg=cpair(colors.black,colors.green)}
+    TextBox{parent=summary,x=1,y=2,text=" Resumo",fg_bg=cpair(colors.black,colors.green)}
 
     local setting_list = ListBox{parent=sum_c_1,x=1,y=1,height=12,width=49,scroll_height=100,fg_bg=bw_fg_bg,nav_fg_bg=g_lg_fg_bg,nav_active=cpair(colors.black,colors.gray)}
 
@@ -408,11 +408,11 @@ function system.create(tool_ctl, main_pane, cfg_sys, divs, ext, style)
         end
     end
 
-    PushButton{parent=sum_c_1,x=1,y=14,text="\x1b Back",callback=back_from_summary,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
-    self.show_key_btn = PushButton{parent=sum_c_1,x=8,y=14,min_width=17,text="Unhide Auth Key",callback=function()self.show_auth_key()end,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg,dis_fg_bg=btn_dis_fg_bg}
-    tool_ctl.settings_apply = PushButton{parent=sum_c_1,x=43,y=14,min_width=7,text="Apply",callback=save_and_continue,fg_bg=cpair(colors.black,colors.green),active_fg_bg=btn_act_fg_bg}
+    PushButton{parent=sum_c_1,x=1,y=14,text="\x1bVoltar",callback=back_from_summary,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
+    self.show_key_btn = PushButton{parent=sum_c_1,x=9,y=14,min_width=21,text="Mostrar Chave de Aut.",callback=function()self.show_auth_key()end,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg,dis_fg_bg=btn_dis_fg_bg}
+    tool_ctl.settings_apply = PushButton{parent=sum_c_1,x=43,y=14,min_width=7,text="Aplicar",callback=save_and_continue,fg_bg=cpair(colors.black,colors.green),active_fg_bg=btn_act_fg_bg}
 
-    TextBox{parent=sum_c_2,x=1,y=1,text="Settings saved!"}
+    TextBox{parent=sum_c_2,x=1,y=1,text="Configura\xe7\xe3o salva!"}
 
     local function go_home()
         main_pane.set_value(1)
@@ -424,9 +424,9 @@ function system.create(tool_ctl, main_pane, cfg_sys, divs, ext, style)
     end
 
     PushButton{parent=sum_c_2,x=1,y=14,min_width=6,text="Home",callback=go_home,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
-    PushButton{parent=sum_c_2,x=44,y=14,min_width=6,text="Exit",callback=exit,fg_bg=cpair(colors.black,colors.red),active_fg_bg=cpair(colors.white,colors.gray)}
+    PushButton{parent=sum_c_2,x=44,y=14,min_width=6,text="Sair",callback=exit,fg_bg=cpair(colors.black,colors.red),active_fg_bg=cpair(colors.white,colors.gray)}
 
-    TextBox{parent=sum_c_3,x=1,y=1,height=2,text="The old config.lua and coord.settings files will now be deleted, then the configurator will exit."}
+    TextBox{parent=sum_c_3,x=1,y=1,height=2,text="Os arquivos config.lua e coord.settings antigos ser\xe3o exclu\xeddos, e o configurador ser\xe1 encerrado."}
 
     local function delete_legacy()
         fs.delete("/coordinator/config.lua")
@@ -434,12 +434,12 @@ function system.create(tool_ctl, main_pane, cfg_sys, divs, ext, style)
         exit()
     end
 
-    PushButton{parent=sum_c_3,x=1,y=14,min_width=8,text="Cancel",callback=go_home,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
+    PushButton{parent=sum_c_3,x=1,y=14,min_width=8,text="Cancelar",callback=go_home,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
     PushButton{parent=sum_c_3,x=44,y=14,min_width=6,text="OK",callback=delete_legacy,fg_bg=cpair(colors.black,colors.green),active_fg_bg=cpair(colors.white,colors.gray)}
 
-    TextBox{parent=sum_c_4,x=1,y=1,height=5,text="Failed to save the settings file.\n\nThere may not be enough space for the modification or server file permissions may be denying writes."}
+    TextBox{parent=sum_c_4,x=1,y=1,height=5,text="N\xe3o foi poss\xedvel salvar as configura\xe7\xf5es.\n\nPode n\xf5o haver espa\xe7o suficiente ou as permiss\xf5es do servidor podem estar negando grava\xe7\xf5es."}
     PushButton{parent=sum_c_4,x=1,y=14,min_width=6,text="Home",callback=go_home,fg_bg=nav_fg_bg,active_fg_bg=btn_act_fg_bg}
-    PushButton{parent=sum_c_4,x=44,y=14,min_width=6,text="Exit",callback=exit,fg_bg=cpair(colors.black,colors.red),active_fg_bg=cpair(colors.white,colors.gray)}
+    PushButton{parent=sum_c_4,x=44,y=14,min_width=6,text="Sair",callback=exit,fg_bg=cpair(colors.black,colors.red),active_fg_bg=cpair(colors.white,colors.gray)}
 
     --#endregion
 
@@ -527,7 +527,7 @@ function system.create(tool_ctl, main_pane, cfg_sys, divs, ext, style)
             local val = util.strval(raw)
 
             if f[1] == "AuthKey" then val = string.rep("*", string.len(val))
-            elseif f[1] == "LogMode" then val = util.trinary(raw == log.MODE.APPEND, "append", "replace")
+            elseif f[1] == "LogMode" then val = util.trinary(raw == log.MODE.APPEND, "anexar", "substituir")
             elseif f[1] == "TempScale" then
                 val = util.strval(types.TEMP_SCALE_NAMES[raw])
             elseif f[1] == "EnergyScale" then
@@ -541,7 +541,7 @@ function system.create(tool_ctl, main_pane, cfg_sys, divs, ext, style)
             elseif f[1] == "UnitDisplays" and type(cfg.UnitDisplays) == "table" then
                 val = ""
                 for idx = 1, #cfg.UnitDisplays do
-                    val = val .. util.trinary(idx == 1, "", "\n") .. util.sprintf(" \x07 Unit %d - %s", idx, cfg.UnitDisplays[idx])
+                    val = val .. util.trinary(idx == 1, "", "\n") .. util.sprintf(" \x07 Unid %d - %s", idx, cfg.UnitDisplays[idx])
                 end
             end
 
